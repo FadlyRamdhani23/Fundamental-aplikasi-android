e25cb2a67088147821022f26fbddf8d6536525c5
FadlyRamdhani4503 <91235752+FadlyRamdhani4503@users.noreply.github.com>
2023-09-13 11:34:49 +0700

    latihan SearcBar dan Search View

---
+++ app/src/main/java/org/d3if3127/fundamentalaplikasiandroid/MenuActivity.kt
@@ -0,0 +1,28 @@
+package org.d3if3127.fundamentalaplikasiandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import org.d3if3127.fundamentalaplikasiandroid.databinding.ActivityMenuBinding
+
+class MenuActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMenuBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            searchView.setupWithSearchBar(searchBar)
+            searchView
+                .editText
+                .setOnEditorActionListener { textView, actionId, event ->
+                    searchBar.text = searchView.text
+                    searchView.hide()
+                    Toast.makeText(this@MenuActivity, searchView.text, Toast.LENGTH_SHORT).show()
+                    false
+                }
+        }
+    }
+}
